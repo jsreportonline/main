39c19e3a8302caaf776850b373ae8a2d89e7360a
pofider <honza.pofider@seznam.cz>
2016-08-17 19:08:23 +0200

    initial test

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:4.4
+MAINTAINER Jan Blaha
+EXPOSE 5488
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+EXPOSE 5488
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server running  on ${port}`);
+});
