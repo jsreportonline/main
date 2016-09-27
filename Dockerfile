f40a2a49bbffcef7b2fe92fc037de898a0475f14
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-09-27 21:25:35 +0200

    performance testing

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,9 @@ FROM ubuntu:latest
 MAINTAINER Jan Blaha
 EXPOSE 5488
 
-RUN apt-get update && apt-get install -y sudo
-RUN apt-get install -y  curl
-RUN curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-RUN apt-get install -y nodejs docker.io build-essential
+RUN apt-get update && apt-get install -y  curl && \
+    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - && \
+    apt-get install -y nodejs
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
---
+++ performance/run.js
@@ -0,0 +1,139 @@
+const request = require('request-promise')
+const Promise = require('bluebird')
+const fs = require('fs')
+const path = require('path')
+const url = require('url')
+
+//const server = 'http://local.net:5488'
+const server = 'http://jsreportonline-test.net'
+const serverUrl = url.parse(server)
+
+const config = {
+  numberOfAccounts: 5,
+  iterations: 500
+}
+
+var accounts = []
+var templates
+
+const createAccounts = () => {
+  console.log(`creating ${config.numberOfAccounts} accounts`)
+  accounts = new Array(config.numberOfAccounts)
+    .fill(1)
+    .map(() => Math.floor(Math.random() * 100000000))
+    .map((id) => ({
+      username: `${id}@perf.com`,
+      url: `${serverUrl.protocol}//${id}.${serverUrl.hostname}${serverUrl.port ? ':' + serverUrl.port : ''}`,
+      name: id,
+      password: 'password',
+      passwordConfirm: 'password',
+      authHeader: `Basic ${new Buffer(`${id}@perf.com:password`).toString('base64')}`,
+      terms: true
+    }))
+
+  return Promise.map(accounts, (a) => request.post(`${server}/register`, {
+    form: a,
+    simple: false
+  }), { concurrency: 1 })
+}
+
+const caseInvoice = (a) => {
+  return request.post({
+    url: `${a.url}/odata/templates`,
+    body: {
+      content: fs.readFileSync(path.join(__dirname, 'cases', 'invoice', 'content.html')).toString(),
+      helpers: fs.readFileSync(path.join(__dirname, 'cases', 'invoice', 'helpers.js')).toString(),
+      recipe: 'phantom-pdf',
+      engine: 'jsrender',
+      name: 'invoice'
+    },
+    json: true,
+    headers: {
+      'Authorization': a.authHeader
+    }
+  })
+}
+
+const caseScript = (a) => {
+  return request.post({
+    url: `${a.url}/odata/scripts`,
+    body: {
+      content: fs.readFileSync(path.join(__dirname, 'cases', 'script', 'script.js')).toString(),
+      name: 'script'
+    },
+    json: true,
+    headers: {
+      'Authorization': a.authHeader
+    }
+  }).then((body) => {
+    return request.post({
+      url: `${a.url}/odata/templates`,
+      body: {
+        content: fs.readFileSync(path.join(__dirname, 'cases', 'script', 'content.html')).toString(),
+        recipe: 'phantom-pdf',
+        engine: 'handlebars',
+        name: 'script',
+        scripts: [{
+          shortid: body.shortid
+        }]
+      },
+      json: true,
+      headers: {
+        'Authorization': a.authHeader
+      }
+    })
+  })
+}
+
+const cases = [caseInvoice, caseScript]
+
+const casesRun = [{
+  template: {
+    name: 'invoice',
+    recipe: 'phantom-pdf'
+  },
+  data: JSON.parse(fs.readFileSync(path.join(__dirname, 'cases', 'invoice', 'data.json')).toString())
+}, {
+  template: {
+    name: 'script',
+    recipe: 'wkhtmltopdf'
+  }
+}]
+
+const createReports = () => {
+  console.log('creating cases')
+
+  return Promise.all(accounts.map((a) => Promise.all(cases.map((c) => c(a)))))
+}
+
+var renderCounter = 0;
+var errorCounter = 1;
+const run = () => {
+  console.log('rendering reports')
+  return Promise.all(accounts.map((a) => Promise.map(new Array(config.iterations).fill(1), () => request.post({
+      url: `${a.url}/api/report`,
+      body: casesRun[Math.floor(Math.random() * casesRun.length)],
+      json: true,
+      headers: {
+        'Authorization': a.authHeader
+      }
+    }).then((body) => {
+      console.log(++renderCounter)
+    }).catch((e) => {
+      console.log(`error: ${++errorCounter}`)
+    }), { concurrency: 1 })
+  ))
+}
+
+const start = new Date().getTime()
+createAccounts()
+  .then(createReports)
+  .then(run)
+  .then(() => {
+    console.log('done')
+    const elapsedTime = new Date().getTime() - start
+    console.log(`Elapsed time ${elapsedTime} ms`)
+    const numberOfReports = cases.length * accounts.length * config.iterations
+    console.log(`Reports per second ${numberOfReports / (elapsedTime / 1000)}`)
+  })
+  .catch((e) => {throw e})
